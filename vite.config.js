1d4c5558c84bdc3342910dbc6f920c0681d1e572
Deepak Chaudhary <deepakkumar312005@gmail.com>
2025-06-10 08:50:27 +0530

    Added vite config

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    allowedHosts: ['3a35-160-202-36-148.ngrok-free.app'],
+  },
+}) 
